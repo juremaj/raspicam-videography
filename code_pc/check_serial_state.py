44ab02684c1f9b068ff592e2258ba53883934400
juremaj <majnik.jure@gmail.com>
2023-01-09 00:01:45 +0100

    updated pc code

--- code_pc/check_serial_state.py
+++ code_pc/check_serial_state.py
@@ -1,10 +1,22 @@
 import serial
-import time
+import matplotlib.pyplot as plt
 
 def read_serial_data():
     ser = serial.Serial("/dev/ttyACM0", 9600)   #specify your port and braudrate
-    data = ser.read()                         #read byte from serial device
+    data = ser.read()           #read byte from serial device
     print(data)                               #display the read byte
 
-while True:
-    read_serial_data()
+    return data
+
+def plot_all_data(all_data):
+    plt.plot(all_data)
+    plt.show()
+    plt.xlabel('Time(s)')
+
+all_data = []
+
+while len(all_data) < 1000:
+    data = read_serial_data()
+    all_data.append(int(data))
+
+plot_all_data(all_data)
